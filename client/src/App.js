c3a61f92f21d27f29925bcf75cc9b63676bf69f3
Chris Gill <chrisfgill@gmail.com>
2019-03-11 06:01:59 -0400

    blogroll component

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
 
+import BlogRoll from "./blogroll";
+
 class App extends Component {
     render() {
         return (
             <div className="App">
                 <header className="App-header">
-
+                    <BlogRoll/>
                 </header>
             </div>
         );
---
+++ client/src/blogroll.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class BlogRoll extends Component {
+    render() {
+        return (
+            <div>
+                Blog Roll
+            </div>
+        );
+    }
+}
+
+export default BlogRoll;
