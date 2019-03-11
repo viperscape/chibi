5f8437eebf0f178229a755459694302dbe1620b5
Chris Gill <chrisfgill@gmail.com>
2019-03-11 12:36:02 -0400

    handle visual authorization cues

--- client/src/login.js
+++ client/src/login.js
@@ -1,21 +1,81 @@
 import React, { Component } from 'react';
+const config = require("./config.json")
+
+const StatusNone = 0,
+    StatusLogin = 1,
+    StatusAuthorizing = 2,
+    StatusAuthorized = 3,
+    StatusUnauthorized = 4;
 
 class Login extends Component {
     constructor(props) {
         super(props);
-        this.state = {authorized: null};
+        this.state = {status: StatusNone};
+        this.user = React.createRef();
+        this.password = React.createRef();
+
+        this.showLogin = this.showLogin.bind(this);
+        this.login = this.login.bind(this);
     }
 
     login(event)
     {
-        
+        fetch(config.backend.server + "/login/", {
+            method: "POST",
+            body: {"username": this.user.current.value, "password": this.password.current.value }
+        })
+        .then(function(res) {
+            return res.json();
+        })
+        .then(function(res) {
+            this.setState({
+                status: res.authorized? StatusAuthorized:StatusUnauthorized
+            });
+        }.bind(this))
+        .catch(function(err)
+        {
+            console.error(err)
+            this.setState({
+                error: "Server communication error",
+                status: StatusUnauthorized
+            });
+        }.bind(this));
+    }
+
+    showLogin(event)
+    {
+        this.setState({status: StatusLogin})
     }
 
     render() {
         return (
             <div className="Login">
-                {!this.state.authorized && 
-                    <button onClick={this.login}>Login</button>
+                {this.state.status === StatusUnauthorized &&
+                    <div>
+                        <span>Failed to authorize, try again</span>
+                        <br></br>
+                    </div>
+                }
+                {(!this.state.status || this.state.status === StatusUnauthorized) && 
+                    <button onClick={this.showLogin}>Login Now</button>
+                }
+                {this.state.status === StatusLogin && 
+                    <div>
+                        <label>
+                            Username:
+                            <input ref={this.user}/>
+                        </label>
+                        <br></br>
+                        <label>
+                            Password:
+                            <input ref={this.password}/>
+                        </label>
+                        <br></br>
+                        <button onClick={this.login}>Login</button>
+                    </div>
+                }
+                {this.state.status === StatusAuthorizing &&
+                    <span>Authorizing...</span>
                 }
             </div>
         );
--- server/routes.py
+++ server/routes.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, make_response
+from flask import Flask, request, make_response, session
 from flask import Blueprint
 from flask import redirect, url_for
 import json
@@ -6,14 +6,25 @@ from post import Post
 
 routes = Blueprint('routes', __name__)
 
-@routes.route('/blog/')
-def blog():
-    #req = request.get_json()
-    q = Post.query.limit(5)
-    posts = [{"id":post.id, "title": post.title, "body": post.body} for post in q]
-    blog = {"posts":posts}
-    data = json.dumps(blog)
+def allow_cors(data):
     resp = make_response(data)
     resp.headers['Access-Control-Allow-Origin'] = '*'
     return resp
 
+@routes.route('/blog/', methods=["GET"])
+def blogroll():
+    #req = request.get_json()
+    q = Post.query.limit(5)
+    posts = [{"id":post.id, "title": post.title, "body": post.body} for post in q]
+    blog = {"posts":posts}
+    return allow_cors(json.dumps(blog))
+
+@routes.route('/blog/', methods=["POST"])
+def blogedit():
+    return allow_cors("1")
+
+@routes.route('/login/', methods=["POST"])
+def login():
+    print(request)
+    status = {"authorized": session.get("authorized")}
+    return allow_cors(json.dumps(status))
