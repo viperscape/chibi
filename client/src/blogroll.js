d162b77ed46fea0bae0b3945c51614a68e103e62
Chris Gill <chrisfgill@gmail.com>
2019-03-11 06:57:16 -0400

    basic fetch of blog roll

--- client/src/blogroll.js
+++ client/src/blogroll.js
@@ -1,10 +1,48 @@
 import React, { Component } from 'react';
+const config = require("./config.json")
 
 class BlogRoll extends Component {
-    render() {
+    constructor(props)
+    {
+        super(props);
+        this.state = {blog:[]};
+    }
+
+    getBlog()
+    {
+        return fetch(config.backend.server + "/blog/")
+        .then(function(res) {
+            return res.json();
+        })
+        .catch(function(err)
+        {
+            console.log(err)
+        });
+    }
+
+    componentDidMount()
+    {
+        this.getBlog().then(function(res) {
+            this.setState({
+                blog: res.posts
+            });
+        }.bind(this));
+    }
+
+    render() 
+    {
         return (
             <div>
-                Blog Roll
+                <span>Blog Roll</span><br></br>
+                <br></br>
+                <div>
+                    { this.state.blog.map((post, key) => 
+                        <div key={key}>
+                            <div>{post.title}</div>
+                            <div>{post.body}</div><br></br>
+                        </div>
+                    )}
+                </div>
             </div>
         );
     }
--- server/main.py
+++ server/main.py
@@ -1,6 +1,20 @@
-from flask import Flask
+from flask import Flask, request, make_response
+from flask import redirect, url_for
+import json
+
 app = Flask(__name__)
 
-@app.route('/')
+# ignoreme: static files for debugging
+# app = Flask(__name__, static_folder='static', static_url_path='')
+# @app.route('/<path:path>')
+#def static_file(path):
+#    return app.send_static_file(path)
+
+@app.route('/blog/')
 def index():
-    return "1"
+    req = request.get_json()
+    blog = {"posts":[{"title":"post1", "body": "sometext"}, {"title":"post2", "body": "alsosometext"}]}
+    data = json.dumps(blog)
+    resp = make_response(data)
+    resp.headers['Access-Control-Allow-Origin'] = '*'
+    return resp
